56ac2c4f9b235e7087b5c8f9a31dd099ded3420b
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-07-07 10:42:14 +0200

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-const listaDom = document.getElementById("container");
+const containerDom = document.getElementById("container");
 
-for (let i = 0; i <= 100; i++) {
-    containerDom.innerHTML += `<div class="box">${i}</div>`;
-
-}
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('fizzBuzz');
+        containerDom.innerHTML += "<div class= 'box-fizzbuzz'>FizzBuzz</div>";
+    } else if (i % 3 == 0) {
+        console.log('fizz');
+        containerDom.innerHTML += "<div class= 'box-fizz'>Fizz</div>";
+    } else if (i % 5 == 0) {
+        console.log('Buzz');
+        containerDom.innerHTML += "<div class= 'box-buzz'>Buzz</div>";
+    } else {
+        console.log(i);
+        containerDom.innerHTML += "<div class= 'box'>" + i + "</div>";
+    }
+};
